212c7ef361b434388b6f8ff97199b8fc1e6f6254
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-10-25 18:54:28 -0400

    Created most files and folders

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 // This is all just boiler plate for the serve page
-const express = re
+const express = require('express')
+const app = express()
+//method override as a requirement
+const methodOverride = require(`method-override`)
+// const routes = require('./routes/index')
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use('/', routes)
+// This is part of the requirement portion
+app.use(methodOverride('_method'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
+//This is the end of the boiler plate of the server.js file
