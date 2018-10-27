bbea4fabee10d6c8f032df69365241de5275691d
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-10-27 19:15:39 -0400

    still trying to hook up db

--- db/connections.js
+++ db/connections.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/nursing-homes')
+// Do I use this one?
+// mongoose.connect('mongodb://localhost/Project2')
+// mongoose.connection.once('open', () => {
+//     console.log('Mongoose has connected to MongoDB')
+// })
 
-mongoose.connection.once('open', () =>{
-    console.log('Mongoose is connected to MongoDB')
+
+//Or do I use this one?
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+console.log('Connected to MongoDB')
 })
 
 module.exports = mongoose
--- db/seeds.js
+++ db/seeds.js
@@ -3,14 +3,28 @@ const NursingHome = require('../models/NursingHome.js')
 const Patient = require('../models/Patient.js')
 const Drug = require('../models/Drug.js')
 
-//SEED DATA - Nursing Homes
-const nhome1 = new NursingHome({
-    name: "Shady Acres",
-    location: "123 Acorn Street",
-    security: "Low",
-    petsAllowed: true,
-    patients: [Tom, Sally, Eugene] //This shit has the new products that we listed above, they are in an array. this was referenced in the models page for store
+//SEED DATA - Drugs
+const drug1 = new Drug({
+    name: "Triazolam",
+    strengthInMg: 200,
+    routeTaken: "Orally",
+    dailyFrequency: "QID"
 })
+
+const drug2 = new Drug({
+    name: "Captopril",
+    strengthInMg: 1000,
+    routeTaken: "Orally",
+    dailyFrequency: "BID"
+})
+
+const drug3 = new Drug({
+    name: "Omeprazole",
+    strengthInMg: 40,
+    routeTaken: "Orally",
+    dailyFrequency: "QD"
+})
+
 
 
 //SEED DATA - Patients
@@ -19,12 +33,7 @@ const Tom = new Patient({
     lastName: "Matheson",
     age: 68,
     conditions: "Heart Problems",
-    drugs: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Drug' //ONE TO MANY RELATIONSHIP BECAUSE PATIENT CAN HAVE MANY DRUGS
-        }
-    ],
+    drugs: [drug1],
     roomNumber: 238,
 })
 
@@ -43,40 +52,29 @@ const Eugene = new Patient({
     lastName: "Minkler",
     age: 99,
     conditions: "Dementia",
-    drugs: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Drug' //ONE TO MANY RELATIONSHIP BECAUSE PATIENT CAN HAVE MANY DRUGS
-        }
-    ],
+    drugs: [ drug1, drug2, drug3],
     roomNumber: 838,
     pet: "Fish",
     petName: "Artemis"
 })
-//SEED DATA - Drugs
-const drug1 = new Schema({
-    name: "Triazolam",
-    strengthInMg: 200,
-    routeTaken: "Orally",
-    dailyFrequency: "QID"
+
+
+//SEED DATA - Nursing Homes
+const nhome1 = new NursingHome({
+    name: "Shady Acres",
+    location: "123 Acorn Street",
+    security: "Low",
+    petsAllowed: true,
+    patients: [Sally, Eugene] //This shit has the new products that we listed above, they are in an array. this was referenced in the models page for store
 })
 
-const drug2 = new Schema({
-    name: "Captopril",
-    strengthInMg: 1000,
-    routeTaken: "Orally",
-    dailyFrequency: "BID"
+const nhome2 = new NursingHome({
+    name: "Sunset Crossing",
+    location: "444 Sunset Ave",
+    security: "Mid-Level",
+    petsAllowed: false,
+    patients: [Tom,] //This shit has the new products that we listed above, they are in an array. this was referenced in the models page for store
 })
-
-const drug3 = new Schema({
-    name: "Omeprazole",
-    strengthInMg: 40,
-    routeTaken: "Orally",
-    dailyFrequency: "QD"
-})
-
-
-
 
 module.exports = mongoose.model('Drug', Drug)
 
