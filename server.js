399dc5d20b2595b00ed5b1124c06d75a0e868cc9
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-10-29 11:46:27 -0400

    fixed something

--- server.js
+++ server.js
@@ -9,22 +9,6 @@ const mongoose = require('mongoose')
 // mongoose.connect(process.env.MONGODB_URI)
 
 
-// Connection to database
-if (process.env.MONGODB_URI) {
-    mongoose.connect(process.env.MONGODB_URI);
-  }
-  else {
-    mongoose.connect('mongodb://localhost/my-database-name');
-  }
-  mongoose.connection.on('error', function(err) {
-    console.error('MongoDB connection error: ' + err);
-    process.exit(-1);
-    }
-  );
-  mongoose.connection.once('open', function() {
-    console.log("Mongoose has connected to MongoDB!");
-  });
-
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
